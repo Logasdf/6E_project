7840f4eaf52cfad32288325e6109bdbccc38549f
Samuel <kimjinsung319@gmail.com>
2018-11-27 18:46:59 +0900

    I deleted the old project, created a new one and added a client project folder.

---
+++ Client/Assets/Scripts/ServerConnection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+
+public class ServerConnection {
+
+    public static ServerConnection GetInstance()
+    {
+        // not thread-safe yet.
+        // and not sure if it should be a singleton class
+        if(instance == null)
+        {
+            instance = new ServerConnection();
+        }
+
+        return instance;   
+    }
+
+    public void CreateConnection()
+    {
+        clientSocket = new TcpClient(ADDR, PORT);
+        WriteToServer(); // test
+    }
+
+    //test
+    public void WriteToServer()
+    {
+        string msg = "abcde\0";
+        theStream = clientSocket.GetStream();
+        StreamWriter writer = new StreamWriter(theStream);
+        writer.Write(msg);
+        writer.Flush();
+    }
+
+    private const string ADDR = "127.0.0.1";
+    private const int PORT = 9910;
+    private static ServerConnection instance;
+    private TcpClient clientSocket;
+    private NetworkStream theStream;
+
+    private ServerConnection() { }
+	
+}
--- Test/Hello_World.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello, World!");
-	printf("수정");
-	printf("추가된 메시지");
-	printf("New Message");
-	return 0;
-}
