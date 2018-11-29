992f9f3da0c358c55e405fc8e915ed5ffbf71f7b
jins <meg753@naver.com>
2018-11-29 13:46:32 +0900

    Test Commit

---
+++ AsyncNotiEchoServ_win.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <string.h>
+#include <WinSock2.h>
+
+#define BUF_SIZE 100
+
+void CompressSockets(SOCKET hSockArr[], int idx, int total);
+void CompressEvents(WSAEVENT hEventArr[], int idx, int total);
+void ErrorHandling(char* msg);
+
+int main(int argc, char* argv[]) {
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAdr, clntAdr;
+
+	SOCKET hSockArr[WSA_MAXIMUM_WAIT_EVENTS];
+	WSAEVENT hEventArr[WSA_MAXIMUM_WAIT_EVENTS];
+	WSAEVENT newEvent;
+	WSANETWORKEVENTS netEvents;
+
+	int numOfClntSock = 0;
+	int strLen, i;
+	int posInfo, startIdx;
+	int clntAdrLen;
+	char msg[BUF_SIZE];
+
+	if (argc != 2) {
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		ErrorHandling("WSAStartup() Error!");
+	}
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
+		ErrorHandling("bind() Error");
+	}
+
+	if (listen(hServSock, 5) == SOCKET_ERROR) {
+		ErrorHandling("listen() Error");
+	}
+
+	newEvent = WSACreateEvent();
+	if (WSAEventSelect(hServSock, newEvent, FD_ACCEPT) == SOCKET_ERROR) {
+		ErrorHandling("WSAEventSelect() Error");
+	}
+
+	// 중요!!
+	// WSAEventSelect 함수를 통해 상호연결된 소켓과 Event 오브젝트간의 관계를 
+	// 유지하고 관리하기 위해 다음과 같은 코드가 필요
+	hSockArr[numOfClntSock] = hServSock;
+	hEventArr[numOfClntSock] = newEvent;
+	numOfClntSock++;
+
+	while (1) 
+	{
+		posInfo = WSAWaitForMultipleEvents(
+			numOfClntSock, hEventArr, FALSE, WSA_INFINITE, FALSE);
+		startIdx = posInfo - WSA_WAIT_EVENT_0;
+
+		for (i = startIdx; i < numOfClntSock; ++i) {
+			int sigEventIdx = WSAWaitForMultipleEvents(1, &hEventArr[i], TRUE, 0, FALSE);
+			if ((sigEventIdx == WSA_WAIT_FAILED || sigEventIdx == WSA_WAIT_TIMEOUT)) {
+				continue;
+			}
+			else {
+				sigEventIdx = i;
+				WSAEnumNetworkEvents(hSockArr[sigEventIdx], hEventArr[sigEventIdx], &netEvents);
+				if (netEvents.lNetworkEvents & FD_ACCEPT) {
+					// 연결요청시
+					if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0) {
+						puts("Accept Error");
+						break;
+					}
+					clntAdrLen = sizeof(clntAdr);
+					hClntSock = accept(hSockArr[sigEventIdx], (SOCKADDR*)&clntAdr, &clntAdrLen);
+					newEvent = WSACreateEvent();
+					WSAEventSelect(hClntSock, newEvent, FD_READ | FD_CLOSE);
+
+					hSockArr[numOfClntSock] = hClntSock;
+					hEventArr[numOfClntSock] = newEvent;
+					numOfClntSock++;
+					puts("Connected new Client...");
+				}
+
+				if (netEvents.lNetworkEvents & FD_READ) {
+					// 데이터 수신시
+					if (netEvents.iErrorCode[FD_READ_BIT] != 0) {
+						puts("Read Error");
+						break;
+					}
+					strLen = recv(hSockArr[sigEventIdx], msg, sizeof(msg), 0);
+					msg[strLen] = 0;
+					printf("[Message From Client] : %s\n", msg);
+					send(hSockArr[sigEventIdx], msg, strLen, 0);
+				}
+
+				if (netEvents.lNetworkEvents & FD_CLOSE) {
+					// 연결종료 요청시
+					if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0) {
+						puts("Close Error");
+						break;
+					}
+					WSACloseEvent(hEventArr[sigEventIdx]);
+					closesocket(hSockArr[sigEventIdx]);
+					numOfClntSock--;
+					CompressSockets(hSockArr, sigEventIdx, numOfClntSock);
+					CompressEvents(hEventArr, sigEventIdx, numOfClntSock);
+				}
+			}
+		}
+	}
+	WSACleanup();
+	return 0;
+}
+
+void CompressSockets(SOCKET hSockArr[], int idx, int total) {
+	int i;
+	for (i = idx; i < total; ++i) {
+		hSockArr[i] = hSockArr[i + 1];
+	}
+}
+
+void CompressEvents(WSAEVENT hEventArr[], int idx, int total) {
+	int i;
+	for (i = idx; i < total; ++i) {
+		hEventArr[i] = hEventArr[i + 1];
+	}
+}
+
+void ErrorHandling(char* msg) {
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
